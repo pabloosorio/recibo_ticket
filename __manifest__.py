eeb65ad9806fdd464d4e98422e08c72883cc0a7b
pabloosorio <pablo.osorio@xmarts.com>
2018-07-04 12:27:40 -0500

    formato de recibo para ticket

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Ticket - Recibo de compra',
+    'version': '1.0',
+    'summary': 'Personalizacion de ANA PEREZ',
+    'description': 'Modificacion del recibo de compra',
+    'category': 'Personalizacion',
+    'author': 'Pablo Osorio',
+    'website': 'www.xmarts.com',
+    'depends': [
+        'sale',
+                ],
+    'data': [
+        'reports/report_recibo_ticket.xml',
+        'reports/reports_menu.xml'
+    ],
+    'installable': True,
+    'auto_install': False,
+}
